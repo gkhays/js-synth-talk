57eddffa626bbafde8f08a49c319152484f3a6a4
Garve Hays <gkhays@gmail.com>
2019-05-25 01:05:22 -0500

    Add different voices

--- app.js
+++ app.js
@@ -2,17 +2,26 @@ let touchEvent = 'ontouchstart' in window ? 'touchstart' : 'click';
 
 $(document).ready(function() {
 	$('#message').focus();
+
+	speechSynthesis.onvoiceschanged = function() {
+		//var voiceList = speechSynthesis.getVoices();
+		var voiceList = $('#voices');
+		if (voiceList.find('option').length == 0) {
+			speechSynthesis.getVoices().forEach(function(voice, index) {
+				// var option = $('<option>').val(index).html(voice.name);
+				// voiceList.append(option);
+				$('#voices').append($('<option>').val(index).html(voice.name));
+			});
+		}
+	}
+
   $('#go').click(function() {
   	var msg = $("#message").val();
-  	var talker = new SpeechSynthesisUtterance(msg);
-  	window.speechSynthesis.speak(talker);
-  	console.log("button was pushed");
+		var talker = new SpeechSynthesisUtterance();
+		var voices = speechSynthesis.getVoices();
+		// talker.voice = $('#voices option:selected');
+		talker.voice = voices[$('#voices').val()];
+		talker.text = msg;
+		window.speechSynthesis.speak(talker);
   });
-  // Looks like we don't need this.
-  /*$('#go').on(touchEvent, function() {
-  	var msg = $("#message").val();
-  	var talker = new SpeechSynthesisUtterance(msg);
-  	window.speechSynthesis.speak(talker);
-  	console.log("button was pushed");
-  });*/
 });
